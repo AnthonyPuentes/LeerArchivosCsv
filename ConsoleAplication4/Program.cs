39288034d25e87841538d6cbc474afc47a7a6514
AnthonyPuentes <Randy1892@outlook.com>
2013-10-17 18:52:58 -0500

    trabajo en el salon

---
+++ ConsoleAplication4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+class Programa
+{
+
+public static void Main(string[] args)
+{
+
+CSV csv = new CSV();
+csv.leerArchivo();
+csv.VerificaArchivo();
+List<string[]> miFila = csv.ObtenerContenido();
+
+foreach (string[] array in miFila)
+{
+
+Console.WriteLine("id: " + array[0] + "\nNombre: " + array[1] + "\nDirección: " + array[2] + "\nTelefono: " + array[3]);
+
+}
+
+Console.ReadKey();
+}
+
+}
+}
